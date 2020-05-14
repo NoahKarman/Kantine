c1cb3c428d7a25a605fb907f2b8d0426dfd74639
Redeouan <58506963+Redeouan@users.noreply.github.com>
2020-05-14 17:32:56 +0200

    Opgave 2

--- KantineSimulatieBasis/src/main/java/Datum.java
+++ KantineSimulatieBasis/src/main/java/Datum.java
@@ -8,6 +8,16 @@ public class Datum {
 	 * Constructor
 	 */
 	// TODO
+	public Datum (int dag,int maand,int jaar){
+
+		this.dag = dag;
+		this.maand = maand;
+		this.jaar = jaar;
+	}
+
+	public Datum (){
+		System.out.println();
+	}
 
 	public boolean bestaatDatum(int dag, int maand, int jaar) {
 		// TODO
@@ -19,6 +29,32 @@ public class Datum {
 	 *
 	 * @return Geboortedatum
 	 */
+	public int getDag() {
+		return dag;
+	}
+
+	public void setDag(int dag) {
+		this.dag = dag;
+	}
+
+	public int getMaand() {
+		return maand;
+	}
+
+	public void setMaand(int maand) {
+		this.maand = maand;
+	}
+
+	public int getJaar() {
+		return jaar;
+	}
+
+	public void setJaar(int jaar) {
+		this.jaar = jaar;
+	}
+
+
+
 	public String getDatumAsString() {
 		// TODO
 		return "";
