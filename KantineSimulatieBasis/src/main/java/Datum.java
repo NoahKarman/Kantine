bae58896f5e7c3fc017da7ca012e92c3e45c4dd0
Redeouan <58506963+Redeouan@users.noreply.github.com>
2020-05-15 14:16:23 +0200

    opgave 2b

--- KantineSimulatieBasis/src/main/java/Datum.java
+++ KantineSimulatieBasis/src/main/java/Datum.java
@@ -9,19 +9,88 @@ public class Datum {
 	 */
 	// TODO
 	public Datum (int dag,int maand,int jaar){
-
+if (bestaatDatum(dag,maand,jaar)){
 		this.dag = dag;
 		this.maand = maand;
 		this.jaar = jaar;
-	}
+	} else {
+		this.dag = 0;
+		this.maand = 0;
+		this.jaar = 0;
+	} }
+
 
 	public Datum (){
-		System.out.println();
+		this.dag = 0;
+		this.maand = 0;
+		this.jaar = 0;
 	}
 
 	public boolean bestaatDatum(int dag, int maand, int jaar) {
-		// TODO
-		return false;
+		// schikkeljaarCorrect gebruik je alleen in deze methode dus die plaats je niet boven als een variabelen.
+		boolean dagCorrect, maandCorrect , jaarCorrect , schrikkeljaarCorrect;
+		// Manden die eindigen op 31 dagen
+		switch (maand){
+			case 1:
+			case 3:
+			case 5:
+			case 7:
+			case 8:
+			case 10:
+			case 12:
+
+
+				if (dag>=1 && dag <=31){
+					dagCorrect = true;
+				}else{
+					return false;
+				}
+				break;
+			// manden die eindigen op 30 dagen
+			case 4:
+			case 6:
+			case 9:
+			case 11:
+				if (dag>=1 && dag <=30){
+					dagCorrect = true;
+				}else{
+					return false;
+				}
+				break;
+				// 4 en 400 is well correct , en 100 niet
+			// Als het schikkeljaar is feb 29 dagen
+			case 2:
+				if (jaar % 4 == 0 || jaar % 100!= 0 || jaar % 400 == 0 ){
+					schrikkeljaarCorrect = true;
+				} else {
+					schrikkeljaarCorrect = false;
+				}
+				if (schrikkeljaarCorrect){
+					if (dag >= 1 && dag <= 29 ){
+						dagCorrect = true;
+					}else{
+						return false;
+					}}else{
+					if (dag >= 1 && dag <= 28){
+						dagCorrect = true;
+					}else{
+						return false;
+					}
+				}
+				break;
+		}//end switch
+		if (maand >= 1 && maand <= 12 ){
+			maandCorrect = true;
+		}else{
+			return false;
+		}
+		if (jaar >= 1900 && jaar <= 2100){
+			jaarCorrect = true;
+		}else{
+			return false;
+		}
+		// Als alle eisen voldoen van het gehele methode dan return die true.
+		return true;
 	}
 
 	/**
